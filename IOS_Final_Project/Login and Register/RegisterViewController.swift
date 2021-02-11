96c1d1d0bbe0bff3c6f0fa7deddef99243d4bfc7
simranjots <simmusimsim@gmail.com>
2021-02-11 01:13:59 -0500

    Admin login done

--- IOS_Final_Project/Login and Register/RegisterViewController.swift
+++ IOS_Final_Project/Login and Register/RegisterViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 import Firebase
 import FirebaseFirestore
 import Toast_Swift
+import iOSDropDown
 
 class RegisterViewController: UIViewController {
 
@@ -13,14 +14,19 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var Phone: UITextField!
     @IBOutlet weak var Address: UITextField!
     @IBOutlet weak var RgtBtn: UIButton!
+    @IBOutlet weak var RstOwner: DropDown!
+    
     
      
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // The list of array to display. Can be changed dynamically
+        RstOwner.optionArray = ["Yes", "No"]
+        RstOwner.selectedRowColor = .secondarySystemBackground
         // Do any additional setup after loading the view.
-    }
+        
+           }
     override func viewWillAppear(_ animated: Bool) {
         
     }
@@ -28,6 +34,28 @@ class RegisterViewController: UIViewController {
         let n = name.text
         let p = Phone.text
         let a = Address.text
+        // The the Closure returns Selected Index and String
+        RstOwner.didSelect{(selectedText , index ,id) in
+        self.RstOwner.text = "Selected String: \(selectedText) \n index: \(index)"
+      }
+        let rstown = RstOwner.text
+        
+        if n != "" && p != "" && a != "" && rstown != "" && Email.text != "" && Password.text != ""
+        {
+            
+        if rstown == "Yes" {
+            let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "restDetails") as! RestaurantRegisterVC
+            vc.id = p
+            vc.name = n
+            vc.phone = p
+            vc.address = a
+            vc.pass = Password.text
+            vc.ema = Email.text
+            vc.rstOwner = rstown
+            self.present(vc, animated: true, completion: nil)
+        }else{
+
         let signUpManager = FirebaseAuthManager()
     
             if let email = Email.text, let password = Password.text {
@@ -42,7 +70,8 @@ class RegisterViewController: UIViewController {
                             "Email":email,
                             "Password":password,
                             "Phone":p,
-                            "Address":a]
+                            "Address":a,
+                            "Rest_ownr":rstown]
                         db.collection("Users").document(p!).setData(datas as [String : Any])
                            
                         self.view.makeToast(message)
@@ -56,5 +85,12 @@ class RegisterViewController: UIViewController {
                 
             }
     }
+    } else{
+        
+        self.view.makeToast("Please Enter all the feilds ")
+        
+    }
+    }
+   
    
 }
---
+++ IOS_Final_Project/Login and Register/RestaurantRegisterVC.swift
@@ -0,0 +1,77 @@
+
+import UIKit
+import Firebase
+import FirebaseFirestore
+import Toast_Swift
+import iOSDropDown
+class RestaurantRegisterVC: UIViewController {
+
+    @IBOutlet weak var rstName: UITextField!
+    @IBOutlet weak var rstLocation: UITextField!
+    @IBOutlet weak var nmbOfLoc: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+          stepper.wraps = true
+          stepper.autorepeat = true
+          stepper.maximumValue = 10
+    }
+    var id :String?
+    var name : String?
+    var ema : String?
+    var pass : String?
+    var phone : String?
+    var address : String?
+    var rstOwner : String?
+    var rstNm : String?
+    var rstLc : String?
+    var rstNum : String?
+    
+    
+    @IBAction func addLoc(_ sender: UIStepper!) {
+        nmbOfLoc.text = String(Int(sender.value))    }
+    
+    
+    @IBAction func registerRest(_ sender: Any) {
+       
+        rstNm = rstName.text
+        rstLc = rstLocation.text
+        rstNum = nmbOfLoc.text
+        
+        let signUpManager = FirebaseAuthManager()
+    
+            if let email = ema, let password = pass {
+                signUpManager.createUser(email: email, password: password) {[weak self] (success,message) in
+                    guard let `self` = self else { return }
+                    
+                    if (success) {
+                        
+                        let db = Firestore.firestore()
+                        let datas = ["Id":self.id,
+                                     "Name": self.name,
+                                     "Email":self.ema,
+                                     "Password":self.pass,
+                                     "Phone":self.phone,
+                                     "Address":self.address,
+                                     "Rest_ownr":self.rstOwner,
+                                     "Rest_Name":self.rstNm,
+                                     "Rest_Loc":self.rstLc,
+                                     "Rest_NumofLoc":self.rstNum]
+                        db.collection("Owners").document(self.phone!).setData(datas as [String : Any])
+                           
+                        self.view.makeToast(message)
+                    } else {
+                        self.view.makeToast(message)
+                    }
+                    
+                   
+                
+                }
+                
+            }
+    }
+    
+}
